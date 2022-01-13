c8f85ac89549754a803324fef542e58dd70a0bd8
Guilherme dos Santos <guilherme@guisantos.com.br>
2022-01-12 21:05:22 -0300

    Init

---
+++ iOSys - Company Finder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSys - Company Finder
+//
+//  Created by Guilherme de Assis dos Santos on 12/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
